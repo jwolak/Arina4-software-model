2f80f3b1975eaffab5c9f5010a764df5384dac1c
Janusz <januszvdm@gmail.com>
2026-06-19 17:57:10 +0200

    Add unit tests for ArithmeticAndLogicalUnit initialization and HerkusBus interactions

--- tests/src/ArithmeticAndLogicalUnitTest.cpp
+++ tests/src/ArithmeticAndLogicalUnitTest.cpp
@@ -34,10 +34,15 @@
 
 #include "ALUExecutorMock.h"
 #include "ArithmeticAndLogicalUnit/ArithmeticAndLogicalUnit.h"
+#include "Common/ALU/AluReplyMessage.h"
+#include "Common/ALU/AluRequestMessage.h"
+#include "Common/HerkusBusTopics.h"
 #include "MockHerkusBus.h"
+#include "spdlog/spdlog.h"
 
 namespace arithmetic_and_logical_unit_test {
 
+    using namespace Arina4SoftwareModel;
     using namespace Arina4SoftwareModel::ArithmeticAndLogicalUnit;
     using namespace Arina4SoftwareModel::ArithmeticAndLogicalUnit::ALUExecutor;
     using namespace Herkus;
@@ -68,6 +73,46 @@ namespace arithmetic_and_logical_unit_test {
 
     TEST_F(ArithmeticAndLogicalUnitTest, Not_Initialized_And_Inialiazed_Status_As_False_Returned) {
         EXPECT_FALSE(arithmetic_and_logical_unit.GetIsInitialized());
+    }
+
+    TEST_F(ArithmeticAndLogicalUnitTest, Initialize_Should_Subscribe_To_ALU_Topic_On_HerkusBus) {
+        EXPECT_CALL(*herkus_bus_mock, Subscribe(Common::HerkusBusTopics::kAluTopic, _)).Times(1);
+        EXPECT_TRUE(arithmetic_and_logical_unit.Initialize());
+    }
+
+    TEST_F(ArithmeticAndLogicalUnitTest, Initialize_Should_Set_IsInitialized_To_True) {
+        EXPECT_CALL(*herkus_bus_mock, Subscribe(Common::HerkusBusTopics::kAluTopic, _)).Times(1);
+        arithmetic_and_logical_unit.Initialize();
+        EXPECT_TRUE(arithmetic_and_logical_unit.GetIsInitialized());
+    }
+
+    TEST_F(ArithmeticAndLogicalUnitTest, Initialize_Should_Publish_ALU_Response_On_HerkusBus_When_ALU_Request_Received) {
+        Common::ALU::AluRequestMessage alu_request_message{"ADD", 5, 3, 1};
+        Common::ALU::AluReplyMessage alu_reply_message{"ADD", 1};
+
+        EXPECT_CALL(*herkus_bus_mock, Subscribe(Common::HerkusBusTopics::kAluTopic, _))
+            .WillOnce(Invoke([&](const std::string& topic, Herkus::subscriber_callback sub_callback) {
+                nlohmann::json message_payload = alu_request_message;
+                sub_callback(topic, message_payload);
+            }));
+
+        EXPECT_CALL(*alu_executor_mock, Execute("ADD", 5, 3)).WillOnce(Return(alu_reply_message));
+        EXPECT_CALL(*herkus_bus_mock, Publish(Common::HerkusBusTopics::kAluTopic, nlohmann::json(alu_reply_message))).Times(1);
+
+        arithmetic_and_logical_unit.Initialize();
+    }
+
+    TEST_F(ArithmeticAndLogicalUnitTest, Initialize_Should_Handle_ALU_Request_Parsing_Error_Gracefully) {
+        EXPECT_CALL(*herkus_bus_mock, Subscribe(Common::HerkusBusTopics::kAluTopic, _))
+            .WillOnce(Invoke([&](const std::string& topic, Herkus::subscriber_callback sub_callback) {
+                nlohmann::json invalid_message_payload = {{"invalid_field", "invalid_value"}};
+                sub_callback(topic, invalid_message_payload);
+            }));
+
+        EXPECT_CALL(*alu_executor_mock, Execute(_, _, _)).Times(0);
+        EXPECT_CALL(*herkus_bus_mock, Publish(_, _)).Times(0);
+
+        arithmetic_and_logical_unit.Initialize();
     }
 
 }  // namespace arithmetic_and_logical_unit_test
